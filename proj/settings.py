566673e4780d163d58847f0575439fe5da0ed3ab
pio1755 <pio1755@gmail.com>
2021-10-20 17:32:55 +0200

    static

--- proj/settings.py
+++ proj/settings.py
@@ -144,7 +144,7 @@ LOCALE_PATHS = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'public/uploads/')
