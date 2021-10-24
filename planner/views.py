84a42f9e5b415c71c159f5538c76413c57eecea1
pio1755 <pio1755@gmail.com>
2021-10-24 14:30:47 +0200

    settings

--- planner/views.py
+++ planner/views.py
@@ -1,18 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import UpdateView
 
 from accounts.forms import NewUserForm
 from .forms import CustomSettingsForm, CustomUserForm
 from .models import CustomSettings
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, get_user_model
 from django.contrib import messages
 from accounts.models import CustomUser
-
-
 
 
 def register_request(request):
@@ -38,13 +38,19 @@ class CustomSettingsUpdateView(UpdateView):  # noqa: D101
     def get_object(self, queryset=None):  # noqa: D102
         return self.model.objects.first()
 
+
+@method_decorator(login_required(), name='dispatch')
 class CustomUserUpdateView(UpdateView):  # noqa: D101
 
-    model = CustomUser
+    model = get_user_model()
     template_name = 'user_profile.html'
     form_class = CustomUserForm
     success_url = reverse_lazy('user_profile')
+    pk = None
 
     def get_object(self, queryset=None):  # noqa: D102
+        return get_user_model().objects.get(pk=self.pk)
 
-        return self.model.objects.first()
+    def dispatch(self, request, *args, **kwargs):  # noqa: D102
+        self.pk = request.user.pk
+        return super().dispatch(request, *args, **kwargs)
