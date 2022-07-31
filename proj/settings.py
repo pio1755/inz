ae840b31c89ec21cf55f7c7cbaa9593a5f485eef
pio1755 <pio1755@gmail.com>
2022-07-31 23:15:02 +0200

    planer2

--- proj/settings.py
+++ proj/settings.py
@@ -169,6 +169,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '/static/'),
 ]
@@ -178,11 +179,13 @@ LOCALE_PATHS = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'apps_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',)
+
+# STATICFILES_FINDERS = (
+#     'django.contrib.staticfiles.finders.FileSystemFinder',
+#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',)
+#
 
 
 MEDIA_URL = '/media/'
