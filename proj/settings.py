dccd0f23be2eeedd87c5c59d2b4fc457ac8b0fb9
pio1755 <pio1755@gmail.com>
2025-05-19 21:20:58 +0200

    ostatnie poprawki

--- proj/settings.py
+++ proj/settings.py
@@ -171,7 +171,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/static/'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 LOCALE_PATHS = [
@@ -179,7 +179,7 @@ LOCALE_PATHS = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
 
 # STATICFILES_FINDERS = (
